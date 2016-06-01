8d7de0cb7b1f61c00cb684044095e5c4bcf9a84b
Sam Greenlee <sam.a.greenlee@gmail.com>
2016-06-01 11:44:15 -0400

    React is up

---
+++ frontend/components/app.jsx
@@ -0,0 +1,19 @@
+var React = require("react");
+
+var App = React.createClass({
+
+  getInitialState: function() {
+    return {};
+  },
+
+  render: function() {
+    return (
+
+      <div>Welcome to the app!</div>
+
+    );
+  }
+
+});
+
+module.exports = App;
