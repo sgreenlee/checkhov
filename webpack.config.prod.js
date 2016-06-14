e7ce7e75538af47b6f3e7119f0e11ca4dbbd9071
Sam Greenlee <sam.a.greenlee@gmail.com>
2016-06-14 18:11:31 -0400

    Removes React minification warning

---
+++ webpack.config.prod.js
@@ -0,0 +1,41 @@
+var webpack = require("webpack");
+var path = require("path");
+
+module.exports = {
+  context: __dirname,
+  entry: "./frontend/checkhov.jsx",
+  output: {
+    path: path.join(__dirname, 'app', 'assets', 'javascripts'),
+    filename: "bundle.js",
+    devtoolModuleFilenameTemplate: '[resourcePath]',
+    devtoolFallbackModuleFilenameTemplate: '[resourcePath]?[hash]'
+  },
+  plugins:[
+    new webpack.DefinePlugin({
+      'process.env':{
+        'NODE_ENV': JSON.stringify('production')
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress:{
+        warnings: true
+      }
+    })
+  ],
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel',
+        query: {
+          presets: ['react']
+        }
+      }
+    ]
+  },
+  devtool: 'source-maps',
+  resolve: {
+    extensions: ["", ".js", ".jsx" ]
+  }
+};
